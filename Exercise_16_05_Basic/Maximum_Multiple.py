dfa487ceda71e09210e72606aa45ba84f7a07e50
ValsotevS <135729337+ValsotevS@users.noreply.github.com>
2025-06-01 18:33:34 +0300

    delete duplicated folder
    removing the duplicated folder as I created new with better naming

--- Exercise_16_05_Basic/Maximum_Multiple.py
+++
@@ -1,7 +0,0 @@
-divisor = int(input())
-boundary = int(input())
-
-for current_number in range(boundary, divisor -1, -1):
-    if current_number % divisor == 0:
-        break
-print(current_number)
