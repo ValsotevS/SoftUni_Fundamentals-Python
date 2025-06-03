e00dd00b168f3f99837446de2450bdc041834844
ValsotevS <135729337+ValsotevS@users.noreply.github.com>
2025-06-03 17:40:15 +0300

    update

--- Lecture_09_Exercise_Data_Type_and_Variables/print_part_of_the_ASCII_table.py
+++ Lecture_09_Exercise_Data_Type_and_Variables/print_part_of_the_ASCII_table.py
@@ -1,8 +1,9 @@
 char_index_start = int(input())
 char_index_final = int(input())
-final_characters = ""
+
 for i in range(char_index_start, char_index_final + 1):
     character = chr(i)
-    final_characters += character + " "
-
-print(final_characters)
+    if i == char_index_final:
+        print(character)
+    else:
+        print(character, end= " ")
