f781b5c4943bdbb09aa97f5b28114b822ad0ef81
ValsotevS <135729337+ValsotevS@users.noreply.github.com>
2025-06-13 23:46:15 +0300

    bread_factory_cleaner_solutions

--- Lecture_12_List_Basic_Exercises/bread_factory_2.py
+++ Lecture_12_List_Basic_Exercises/bread_factory_2.py
@@ -1,28 +1,31 @@
+MAX_ENERGY = 100
+ENERGY_COST_PER_ORDER = 30
+ENERGY_RECOVERY_WHEN_TIRED = 50
+
 my_energy = 100
 my_coins = 100
-bakery_Closed = False
 events = input().split("|")
 
 for event in events:
-    current_even_list = event.split("-")
-    type_of_event = current_even_list[0]
-    value_of_event = int(current_even_list[1])
+    current_event_list = event.split("-")
+    type_of_event = current_event_list[0]
+    value_of_event = int(current_event_list[1])
 
     if type_of_event == "rest":
         energy_gain = value_of_event
-        actual_energy_gain = min(energy_gain, 100 - my_energy)
+        actual_energy_gain = min(energy_gain, MAX_ENERGY - my_energy)
         my_energy += actual_energy_gain
         print(f"You gained {actual_energy_gain} energy.")
         print(f"Current energy: {my_energy}.")
 
     elif type_of_event == "order":
-        if my_energy >= 30:
+        if my_energy >= ENERGY_COST_PER_ORDER:
             my_coins += value_of_event
-            my_energy -= 30
+            my_energy -= ENERGY_COST_PER_ORDER
             print(f"You earned {value_of_event} coins.")
         else:
-            energy_gain = 50
-            actual_energy_gain = min(energy_gain, 100 - my_energy)
+            energy_gain = ENERGY_RECOVERY_WHEN_TIRED
+            actual_energy_gain = min(energy_gain, MAX_ENERGY - my_energy)
             my_energy += actual_energy_gain
 
             print("You had to rest!")
@@ -31,9 +34,7 @@ for event in events:
         if my_coins < value_of_event:
             print(f"Closed! Cannot afford {type_of_event}.")
             break
-        if my_coins >= value_of_event:
-            my_coins -= value_of_event
-            print(f"You bought {type_of_event}.")
+
         else:
             my_coins -= value_of_event
             print(f"You bought {type_of_event}.")
